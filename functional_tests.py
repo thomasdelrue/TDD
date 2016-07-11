11768a507e60ee55a1808eefff3390d9e000c077
thomas delrue <thomasdelrue@gmail.com>
2016-07-11 14:16:13 +0200

    Refactor FT: helper function

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 		
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
@@ -35,11 +40,8 @@ class NewVisitorTest(unittest.TestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock food" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy peacock food')
 		
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock food', [row.text for row in rows])
-
 		# There is still a text box inviting her to add another item. She
 		# enters "Bring peacock food to the shelter" (Edith is very methodical)
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -47,11 +49,8 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		
 		# The page updates again, and now shows both items on her list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock food', [row.text for row in rows])
-		self.assertIn('2: Bring peacock food to the shelter', [row.text for row in rows])
-
+		self.check_for_row_in_list_table('1: Buy peacock food')
+		self.check_for_row_in_list_table('2: Bring peacock food to the shelter')
 		
 		# Edith wonders whether the site will remember the list. The she sees
 		# that the site has generated a unique URL for her -- there is some
