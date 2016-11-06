a557610c4d5647c6bd302484fe6072337e5da861
thomas delrue <thomasdelrue@gmail.com>
2016-11-06 15:51:02 +0100

    added logout functionality. end chapter 16

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
 
 import accounts.views
+import django.contrib.auth.views
+
 
 urlpatterns = [
 	url(r'^login$', accounts.views.persona_login, name='persona_login'),
+	url(r'^logout$', django.contrib.auth.views.logout, {'next_page': '/'}, name='logout')
 ]
